2776a736e29e2a7c16e8bec67c229b78d8395214
Ashforfian <yusufkerem2607@gmail.com>
2026-01-04 21:16:19 +0300

    FIX: Tables columns are set to non editable.

--- src/main/java/helper/DbHelper.java
+++ src/main/java/helper/DbHelper.java
@@ -1,14 +1,26 @@
 package helper;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class DbHelper {
-    // Veritabanı adı 'recycleshare' değilse düzelt
     private static final String URL = "jdbc:postgresql://localhost:5432/recycleshare";
     private static final String USER = "postgres";
-    private static final String PASSWORD = "Eses2626.";
+    private static String PASSWORD;
+
+    static {
+        Properties prop = new Properties();
+        try (FileInputStream input = new FileInputStream("config.properties")) {
+            prop.load(input);
+            PASSWORD = prop.getProperty("db.password");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
